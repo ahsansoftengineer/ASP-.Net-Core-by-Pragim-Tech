1c939d34d8a20b0b97c6b5d729ed87ab35c93e19
Muhammad Ahsan <ahsansoftengineer@gmail.com>
2019-09-04 18:32:58 +0500

    Overloaded Method of View has been covered

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,10 +22,29 @@ namespace Core_Demo_2.Controllers
         {
             return _employeeRepository.GetEmployee(101).Name;
         }
+        public ViewResult Detail()
+        {
+            return View();
+        }
         // Changes for Returning the View in MVC Core
-    public ViewResult Details()
-    {
-        return View(_employeeRepository.GetEmployee(101));
-    }
+        public ViewResult Details()
+        {
+            return View(_employeeRepository.GetEmployee(101));
+        }
+        // Example of View to Custom Display of Random Folder
+        public ViewResult PatternView()
+        {
+            return View("PatternView");
+        }
+        // Example of View to Custom Display of Abosulte Path
+        public ViewResult AbsoluteView()
+        { 
+            return View("Absolute/AbsoluteView.cshtml");
+        }
+        // Example of View to Custom Display of Relative Path
+        public ViewResult RelativeView()
+        {
+            return View("../Relative/Relative");
+        }
     }
 }
